e2ee12d6ec5973097267a3d3a5fc26e6af05d964
Antoni Nowicki <atn2122804@gmail.com>
2026-04-01 19:57:37 +0200

    Added languages into src\assets\index.js

---
+++ src/assets/techIndex.js
@@ -0,0 +1,135 @@
+import ASP from "./tech/backend/ASP.PNG";
+import django from "./tech/backend/django.PNG";
+import expressjs from "./tech/backend/expressjs.png";
+import flask from "./tech/backend/flask.PNG";
+import goLang from "./tech/backend/go.PNG";
+import kotlin from "./tech/backend/kotlin.png";
+import backendNodejs from "./tech/backend/nodejs.png";
+import php from "./tech/backend/php.png";
+import python from "./tech/backend/python.png";
+import rust from "./tech/backend/rust.PNG";
+import springBoot from "./tech/backend/springBoot.PNG";
+
+// Database
+import couchdb from "./tech/database/couchdb.PNG";
+import databaseDocker from "./tech/database/docker.png";
+import dynamoDB from "./tech/database/dynamoDB.PNG";
+import firebase from "./tech/database/firebase.png";
+import databaseMongodb from "./tech/database/mongodb.png";
+import mysql from "./tech/database/mysql.png";
+import postgreesql from "./tech/database/postgreesql.png";
+import redis from "./tech/database/redis.PNG";
+
+// Frontend
+import android from "./tech/frontend/android.png";
+import angular from "./tech/frontend/angular.PNG";
+import bootstrap from "./tech/frontend/bootstrap.png";
+import cpp from "./tech/frontend/c++.png";
+import chakra from "./tech/frontend/chakra.png";
+import frontendCss from "./tech/frontend/css.png";
+import graphQL from "./tech/frontend/GraphQL.PNG";
+import frontendHtml from "./tech/frontend/html.png";
+import java from "./tech/frontend/java.png";
+import frontendJavascript from "./tech/frontend/javascript.png";
+import jquery from "./tech/frontend/jquery.png";
+import materialUi from "./tech/frontend/material-ui.png";
+import next from "./tech/frontend/next.PNG";
+import frontendReactjs from "./tech/frontend/reactjs.png";
+import frontendRedux from "./tech/frontend/redux.png";
+import sass from "./tech/frontend/sass.png";
+import frontendTailwind from "./tech/frontend/tailwind.png";
+import frontendThreejs from "./tech/frontend/threejs.svg";
+import frontendTypescript from "./tech/frontend/typescript.png";
+import vue from "./tech/frontend/vue.PNG";
+import wordpress from "./tech/frontend/wordpress.png";
+
+// Others
+import amazon from "./tech/others/amazon.png";
+import digitalocean from "./tech/others/digitalocean.png";
+import othersFigma from "./tech/others/figma.png";
+import othersGit from "./tech/others/git.png";
+import othersGithub from "./tech/others/github.png";
+import heroku from "./tech/others/heroku.png";
+
+// Top-level (already exist but re-export)
+import techCss from "./tech/css.png";
+import techDocker from "./tech/docker.png";
+import techFigma from "./tech/figma.png";
+import techGit from "./tech/git.png";
+import techHtml from "./tech/html.png";
+import techJavascript from "./tech/javascript.png";
+import techMongodb from "./tech/mongodb.png";
+import techNodejs from "./tech/nodejs.png";
+import techReactjs from "./tech/reactjs.png";
+import techRedux from "./tech/redux.png";
+import techTailwind from "./tech/tailwind.png";
+import techThreejs from "./tech/threejs.svg";
+import techTypescript from "./tech/typescript.png";
+
+export {
+  // Backend
+  ASP,
+  django,
+  expressjs,
+  flask,
+  goLang,
+  kotlin,
+  backendNodejs,
+  php,
+  python,
+  rust,
+  springBoot,
+  // Database
+  couchdb,
+  databaseDocker,
+  dynamoDB,
+  firebase,
+  databaseMongodb,
+  mysql,
+  postgreesql,
+  redis,
+  // Frontend
+  android,
+  angular,
+  bootstrap,
+  cpp,
+  chakra,
+  frontendCss,
+  graphQL,
+  frontendHtml,
+  java,
+  frontendJavascript,
+  jquery,
+  materialUi,
+  next,
+  frontendReactjs,
+  frontendRedux,
+  sass,
+  frontendTailwind,
+  frontendThreejs,
+  frontendTypescript,
+  vue,
+  wordpress,
+  // Others
+  amazon,
+  digitalocean,
+  othersFigma,
+  othersGit,
+  othersGithub,
+  heroku,
+  // Top-level
+  techCss,
+  techDocker,
+  techFigma,
+  techGit,
+  techHtml,
+  techJavascript,
+  techMongodb,
+  techNodejs,
+  techReactjs,
+  techRedux,
+  techTailwind,
+  techThreejs,
+  techTypescript,
+};
+
