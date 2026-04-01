190e9202a4d7918034bb5bfb81bee10f0ff2ba08
Antoni Nowicki <atn2122804@gmail.com>
2026-04-01 19:48:44 +0200

    Modified Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,13 +5,27 @@ import { SectionWrapper } from "../hoc";
 import { technologiesFrontend, technologiesBackend, technologiesDatabase, technologiesOthers } from "../constants";
 
 const Tech = () => {
+  const renderTechs = (techs, title) => (
+    <div className="mb-8">
+      <h3 className="text-white text-2xl font-black mb-6 text-center">
+        {title}
+      </h3>
+      <div className='flex flex-row flex-wrap justify-center gap-10'>
+        {techs.map((technology) => (
+          <div className='w-28 h-28' key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
+    <div className='flex flex-col gap-12'>
+      {renderTechs(technologiesFrontend, "Frontend")}
+      {renderTechs(technologiesBackend, "Backend")}
+      {renderTechs(technologiesDatabase, "Database")}
+      {renderTechs(technologiesOthers, "Others")}
     </div>
   );
 };
