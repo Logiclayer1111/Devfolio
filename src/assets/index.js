8e42a093b91cfc198e255a770a3b813c0793dd36
Antoni Nowicki <atn2122804@gmail.com>
2026-04-01 22:30:40 +0200

    Change images

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,49 +20,49 @@ import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
 //backend
-import ASP from "./tech/backend/ASP.PNG";
-import django from "./tech/backend/django.PNG";
+import ASP from "./tech/backend/ASP.png";
+import django from "./tech/backend/django.png";
 import expressjs from "./tech/backend/expressjs.png";
-import flask from "./tech/backend/flask.PNG";
-import goLang from "./tech/backend/go.PNG";
+import flask from "./tech/backend/flask.png";
+import goLang from "./tech/backend/go.png";
 import kotlin from "./tech/backend/kotlin.png";
 import backendNodejs from "./tech/backend/nodejs.png";
 import php from "./tech/backend/php.png";
 import python from "./tech/backend/python.png";
 import rust from "./tech/backend/rust.PNG";
-import springBoot from "./tech/backend/springBoot.PNG";
+import springBoot from "./tech/backend/springBoot.png";
 
 // Database
-import couchdb from "./tech/database/couchdb.PNG";
+import couchdb from "./tech/database/couchdb.png";
 import databaseDocker from "./tech/database/docker.png";
-import dynamoDB from "./tech/database/dynamoDB.PNG";
+import dynamoDB from "./tech/database/dynamoDB.png";
 import firebase from "./tech/database/firebase.png";
 import databaseMongodb from "./tech/database/mongodb.png";
 import mysql from "./tech/database/mysql.png";
 import postgreesql from "./tech/database/postgreesql.png";
-import redis from "./tech/database/redis.PNG";
+import redis from "./tech/database/redis.png";
 
 // Frontend
 import android from "./tech/frontend/android.png";
-import angular from "./tech/frontend/angular.PNG";
+import angular from "./tech/frontend/angular.png";
 import bootstrap from "./tech/frontend/bootstrap.png";
 import cpp from "./tech/frontend/c++.png";
 import chakra from "./tech/frontend/chakra.png";
 import frontendCss from "./tech/frontend/css.png";
-import graphQL from "./tech/frontend/GraphQL.PNG";
+import graphQL from "./tech/frontend/GraphQL.png";
 import frontendHtml from "./tech/frontend/html.png";
 import java from "./tech/frontend/java.png";
 import frontendJavascript from "./tech/frontend/javascript.png";
 import jquery from "./tech/frontend/jquery.png";
 import materialUi from "./tech/frontend/material-ui.png";
-import next from "./tech/frontend/next.PNG";
+import next from "./tech/frontend/next.png";
 import frontendReactjs from "./tech/frontend/reactjs.png";
 import frontendRedux from "./tech/frontend/redux.png";
 import sass from "./tech/frontend/sass.png";
 import frontendTailwind from "./tech/frontend/tailwind.png";
 import frontendThreejs from "./tech/frontend/threejs.png";
 import frontendTypescript from "./tech/frontend/typescript.png";
-import vue from "./tech/frontend/vue.PNG";
+import vue from "./tech/frontend/vue.png";
 import wordpress from "./tech/frontend/wordpress.png";
 
 // Others
